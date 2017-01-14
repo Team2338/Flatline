5f43f08f1cf1847133fdd9da99a50896c63661c6
derekhohos <derekhho@hotmail.com>
2017-01-13 22:32:34 -0600

    Drivetrain, camera processing test

---
+++ src/org/usfirst/frc/team2338/robot/OI.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team2338.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	public static final Joystick leftJoy = new Joystick (0);
+	public static final Joystick rightJoy = new Joystick(1);
+	
+}
+
