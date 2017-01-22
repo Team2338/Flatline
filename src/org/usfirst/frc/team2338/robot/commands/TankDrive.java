acc8eec88807c55e6fabd02bc4940fc418d58519
Occidentalis920 <occidentalis920@gmail.com>
2017-01-22 09:06:43 -0600

    Revert "Revert "Camera processing testing, climber prototype""
    This reverts commit f0c055a51566bb4cef9a4c8be0976ad0617fad43.

--- src/org/usfirst/frc/team2338/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team2338/robot/commands/TankDrive.java
@@ -22,7 +22,7 @@ public class TankDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drivetrain.drive(-OI.leftJoy.getY(), OI.rightJoy.getY());
+//    	Robot.drivetrain.drive(-OI.leftJoy.getY(), OI.rightJoy.getY());
     }
 
     // Make this return true when this Command no longer needs to run execute()
