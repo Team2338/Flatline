2cc3563945aec356fd7f209bbf32ce9dee0eb3c0
Enan456 <enan332@gmail.com>
2017-02-04 13:43:41 -0600

    good code
    8/8

--- src/team/gif/commands/auto/GearGrab.java
+++ src/team/gif/commands/auto/GearGrab.java
@@ -5,21 +5,16 @@ import team.gif.commands.GearIn;
 import team.gif.commands.GearOut;
 import team.gif.commands.drivetrain.Drive;
 
-/**
- *wait command time out amount?
- *enan was here
- */
+//enan was here
 
 public class GearGrab extends CommandGroup {
 
 //	private final WAIT_TIME = 0.0;
     public GearGrab() {
-    	
     	addSequential(new Drive(0.1,0.1));
     	addSequential(new GearOut());
 //    	addSequential(new WaitCommand(WAIT_TIME));
     	addSequential(new GearIn());
     	addSequential(new Drive(-0.1,-0.1));
-   
-    	   }
+    }
 }
--- src/team/gif/commands/drivetrain/Drive.java
+++ src/team/gif/commands/drivetrain/Drive.java
@@ -5,48 +5,35 @@ import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.Command;
 import team.gif.Robot;
 
-/**
- *enan was here
- */
 
+//enan was here
 
 public class Drive extends Command {
 
 	private double left;
 	private double right;
-//                                                                                               n	public static double distance = Talon.getPosition();
+//  public static double distance = Talon.getPosition();
 	
     public Drive(double left, double right) {
         requires(Robot.drivetrain);
         this.left = left;
         this.right = right;
-      
-
-
     }
     
-
-
-    // Called just before this Command runs the first time
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.drivetrain.drive(left, right);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
