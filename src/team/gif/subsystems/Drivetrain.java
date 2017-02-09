8977a4f53be77f9ea8fa98dbadf02b12e8121b31
Eric Butolph <ericbutolph@gmail.com>
2017-02-08 20:43:12 -0600

    transfer code to subsystem

--- src/team/gif/subsystems/Drivetrain.java
+++ src/team/gif/subsystems/Drivetrain.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import team.gif.Globals;
 import team.gif.RobotMap;
 import team.gif.commands.drivetrain.TankDrive;
 
@@ -28,6 +30,8 @@ public class Drivetrain extends Subsystem {
 		super();
 		frontLeft.changeControlMode(TalonControlMode.Position);
 		gyro.calibrate();
+		
+		
 	}
 
 	public void drive(double left, double right) {
@@ -36,6 +40,7 @@ public class Drivetrain extends Subsystem {
 		frontRight.set(right);
 		rearRight.set(right);
 	}
+	
    
 	public void initDefaultCommand() {
     	setDefaultCommand(new TankDrive());
@@ -60,4 +65,108 @@ public class Drivetrain extends Subsystem {
     public void resetGyro() {
     	gyro.reset();
     }
-}
+
+    public final CANTalon frontRightDrive= new CANTalon(RobotMap.frontRightDrive);
+    public final CANTalon frontLeftDrive= new CANTalon(RobotMap.frontLeftDrive);
+
+    {
+	    frontLeftDrive.changeControlMode(TalonControlMode.Position);
+		frontRightDrive.changeControlMode(TalonControlMode.Position);
+		frontLeftDrive.setPID(Globals.frontLeftDriveP, Globals.frontLeftDriveI, Globals.frontLeftDriveD);
+		frontRightDrive.setPID(Globals.frontRightDriveP, Globals.frontRightDriveI, Globals.frontRightDriveD);
+		
+		frontLeftDrive.reverseOutput(true);
+		frontLeftDrive.reverseSensor(false);
+		frontLeftDrive.setAllowableClosedLoopErr(0);
+		frontRightDrive.reverseOutput(true);
+		frontRightDrive.reverseSensor(false);
+		frontRightDrive.setAllowableClosedLoopErr(0);
+    }
+
+	public void setPID(double p, double i, double d) {
+		frontLeftDrive.setPID(p, i, d);
+		frontRightDrive.setPID(p, i, d);
+	}
+	
+	public void setMode(TalonControlMode mode) {
+		frontRightDrive.changeControlMode(mode);
+		frontLeftDrive.changeControlMode(mode);
+	}
+	
+	public void setPosition(double position){
+		frontRightDrive.set(position);
+		frontLeftDrive.set(position);
+	}
+	
+	public double getPGainRight() {
+		return frontRightDrive.getP() * frontRightDrive.getError() / 1;
+	}
+	public double getPGainLeft() {
+		return frontLeftDrive.getP() * frontLeftDrive.getError() / 1;
+	}
+	
+	public double getIGainRight() {
+		return frontRightDrive.GetIaccum() / 1;
+	}
+	public double getIGainLeft() {
+		return frontLeftDrive.GetIaccum() / 1;
+	}
+	
+	public double getDGainRight() {
+		return frontRightDrive.getD() * frontRightDrive.getSpeed() * (frontRightDrive.getError() / 
+				Math.abs(frontRightDrive.getError())) / 1;
+	}
+	public double getDGainLeft() {
+		return frontLeftDrive.getD() * frontLeftDrive.getSpeed() * (frontLeftDrive.getError() / 
+				Math.abs(frontLeftDrive.getError())) / 1;
+	}
+	
+	public double getFGainRight() {
+		return frontRightDrive.getF() * frontRightDrive.getSetpoint() / 1;
+	}
+	public double getFGainLeft() {
+		return frontLeftDrive.getF() * frontLeftDrive.getSetpoint() / 1;
+	}
+	
+	public double getErrorRight() {
+		return frontRightDrive.getError();
+	}
+	public double getErrorLeft() {
+		return frontLeftDrive.getError();
+	}
+	
+	public double getPositionRight() {
+		return frontRightDrive.getPosition();
+	}
+	public double getPositionLeft() {
+		return frontLeftDrive.getPosition();
+	}
+	
+	public double getSetpointRight() {
+		return frontRightDrive.getSetpoint();
+	}
+	public double getSetpointLeft() {
+		return frontLeftDrive.getSetpoint();
+	}
+	
+	public double getIZoneRight() {
+		return frontRightDrive.getIZone();
+	}
+	public double getIZoneLeft() {
+		return frontLeftDrive.getIZone();
+	}
+	
+	public void resetIAccum() {
+		frontRightDrive.clearIAccum();
+		frontLeftDrive.clearIAccum();
+	}
+	
+	public void update() {
+		SmartDashboard.putNumber("frontRightDrive CurrentPos", getPositionRight());
+		SmartDashboard.putNumber("frontRightDrive PGain", getPGainRight());
+		SmartDashboard.putNumber("frontRightDrive Error", getErrorRight());
+		SmartDashboard.putNumber("frontLeftDrive CurrentPos", getPositionLeft());
+		SmartDashboard.putNumber("frontLeftDrive PGain", getPGainLeft());
+		SmartDashboard.putNumber("frontLeftDrive Error", getErrorLeft());
+		}
+	}
