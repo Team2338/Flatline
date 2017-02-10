687b479d0d6593f0c3d782c981f86e84787f1685
Enan456 <enan332@gmail.com>
2017-02-09 20:44:25 -0600

    auto cleanup
    auto cleanup

--- src/team/gif/commands/auto/GearGrab.java
+++ src/team/gif/commands/auto/GearGrab.java
@@ -1,19 +1,21 @@
 package team.gif.commands.auto;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import team.gif.Globals;
 import team.gif.commands.GearRelease;
-import team.gif.commands.drivetrain.GyroDrive;
+import team.gif.commands.drivetrain.GyroTurn;
 
 
 
 public class GearGrab extends CommandGroup {
 	
     public GearGrab() {
-    	addSequential(new GyroDrive(0.1,0.1));
+    	addSequential(new  DriveStraightEnc(Globals.DRIVE_DIST));
 //    	addSequential(new WaitCommand(WAIT_TIME));
+    	addSequential(new GearRelease(false));
     	addSequential(new GearRelease(true));
     	// TODO: Turn 180 degrees
-    	addSequential(new GyroDrive(-0.8,0));
-    	addSequential(new GyroDrive(-0.1,-0.1));
+    	addSequential(new GyroTurn(180));
+    	addSequential(new  DriveStraightEnc(Globals.DRIVE_DIST));
     }
 }
--- src/team/gif/commands/auto/GearShootMobility.java
+++ src/team/gif/commands/auto/GearShootMobility.java
@@ -1,7 +1,7 @@
 package team.gif.commands.auto;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import team.gif.commands.drivetrain.GyroDrive;
+import team.gif.Globals;
 import team.gif.commands.shooter.CameraShoot;
 
 public class GearShootMobility extends CommandGroup {
@@ -10,7 +10,7 @@ public class GearShootMobility extends CommandGroup {
 
     public GearShootMobility() {
 //    	addSequential(new WaitCommand(WAIT_TIME));
-    	addSequential(new GyroDrive(0.25,0.25));
+    	addSequential(new  DriveStraightEnc(Globals.DRIVE_DIST));
 //    	addSequential(new WaitCommand(WAIT_TIME));
         addSequential(new GearGrab());
 //    	addSequential(new WaitCommand(WAIT_TIME));
