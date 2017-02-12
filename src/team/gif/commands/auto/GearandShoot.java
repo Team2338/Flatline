dfc3f8adc2ac8100a6ef6aabcede002797ce3cf8
Gindtis <gindtjoshua@gmail.com>
2017-02-12 13:40:13 -0600

    Auto Gear And Shoot

---
+++ src/team/gif/commands/auto/GearandShoot.java
@@ -0,0 +1,22 @@
+package team.gif.commands.auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import team.gif.Globals;
+import team.gif.commands.GearRelease;
+import team.gif.commands.intake.FeederDrive;
+import team.gif.commands.shooter.RevFlywheel;
+
+public class GearandShoot extends CommandGroup {
+
+    public GearandShoot() {
+    	addSequential(new DriveStraightEnc(Globals.DRIVE_DIST));
+    	addSequential(new GearRelease(true));
+    	addSequential(new DriveStraightEnc(-4960));
+    	addSequential(new GyroTurn(230));
+    	addSequential(new DriveStraightEnc(8000));
+    	addParallel(new RevFlywheel());
+    	addSequential(new WaitCommand(1));
+    	addSequential(new FeederDrive());
+    }
+}
