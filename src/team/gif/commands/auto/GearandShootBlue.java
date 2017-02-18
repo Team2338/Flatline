58761e0e3b3faf1eacca91763ec4eec34df1f37e
Gindtis <gindtjoshua@gmail.com>
2017-02-17 19:05:33 -0600

    Edited Gear and Shoot Auto
    Separate set for Red and Blue alliance.

--- src/team/gif/commands/auto/GearandShootBlue.java
+++ src/team/gif/commands/auto/GearandShootBlue.java
@@ -7,13 +7,13 @@ import team.gif.commands.GearRelease;
 import team.gif.commands.intake.FeederDrive;
 import team.gif.commands.shooter.RevFlywheel;
 
-public class GearandShoot extends CommandGroup {
+public class GearandShootBlue extends CommandGroup {
 
-    public GearandShoot() {
+    public GearandShootBlue() {
     	addSequential(new DriveStraightEnc(Globals.DRIVE_DIST));
     	addSequential(new GearRelease(true));
     	addSequential(new DriveStraightEnc(-4960));
-    	addSequential(new GyroTurn(230));
+    	addSequential(new GyroTurn(240));
     	addSequential(new DriveStraightEnc(8000));
     	addParallel(new RevFlywheel());
     	addSequential(new WaitCommand(1));
