b25fc0fe602f2fc3e75aff0d3c30e6986fc58cfe
Gindtis <gindtjoshua@gmail.com>
2017-02-17 20:55:30 -0600

    A bit of clean up

--- src/team/gif/commands/auto/GearandShootRed.java
+++ src/team/gif/commands/auto/GearandShootRed.java
@@ -2,7 +2,6 @@ package team.gif.commands.auto;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
-import team.gif.Globals;
 import team.gif.commands.GearRelease;
 import team.gif.commands.intake.FeederDrive;
 import team.gif.commands.shooter.RevFlywheel;
