f8226d2120af4fb1455d190377eb086d18040bcc
Occidentalis920 <occidentalis920@gmail.com>
2017-02-23 18:38:38 -0600

    Speed and Clean Vision
    Uses less math for more speed.
    Catches exceptions in a different place and uses less return statements,
    however it may not work.
    TODO: Test

--- src/team/gif/subsystems/Vision.java
+++ src/team/gif/subsystems/Vision.java
@@ -15,39 +15,38 @@ public class Vision extends Subsystem {
 	private Double[] solidity;
 	private double xPixelError;
 	private double yPixelError;
+	private double xDegreeConstant;
+	private double yDegreeConstant;
+	private double halfVertFOV;
+
+	public Vision() {
+		xDegreeConstant = Globals.CAMERA_CENTER_X / Math.tan(Math.toRadians(Globals.CAMERA_HFOV / 2));
+		yDegreeConstant = Globals.CAMERA_CENTER_Y / Math.tan(Math.toRadians(Globals.CAMERA_VFOV / 2));
+		halfVertFOV = Globals.CAMERA_VFOV / 2;
+	}
 
 	public double getXPixelError() {
-		if (Robot.grip.getNumberArray("centerX", centerX) != null) {
-			try {
-				if (centerX.length > 0) {
-					xPixelError = Globals.CAMERA_CENTER_X - centerX[centerX.length - 1];
-				}
-			} catch (ArrayIndexOutOfBoundsException e) {}
-			return xPixelError;
-		}
-		return 0;
+		try {
+			if (centerX.length > 0)
+				xPixelError = Globals.CAMERA_CENTER_X - centerX[centerX.length - 1];
+		} catch (ArrayIndexOutOfBoundsException e) {}
+		return xPixelError;
 	}
 
 	public double getYPixelError() {
-		if (Robot.grip.getNumberArray("centerY", centerY) != null) {
-			try {
-				if (centerY.length > 0) {
-					yPixelError = Globals.CAMERA_CENTER_Y - centerY[centerY.length - 1];
-				}
-				return yPixelError;
-			} catch (ArrayIndexOutOfBoundsException e) {}
-			return yPixelError;
-		}
-		return 0;
+		try {
+			if (centerY.length > 0)
+				yPixelError = Globals.CAMERA_CENTER_Y - centerY[centerY.length - 1];
+		} catch (ArrayIndexOutOfBoundsException e) {}
+		return yPixelError;
 	}
 
 	public double getXDegreeError() {
-		return Math.toDegrees(Math.atan(getXPixelError() / (Globals.CAMERA_CENTER_X / Math.tan(Math.toRadians(Globals.CAMERA_HFOV / 2)))));
+		return Math.toDegrees(Math.atan(getXPixelError() / xDegreeConstant));
 	}
 
-
 	public double getYDegreeError() {
-		return Math.toDegrees(Math.atan(getYPixelError() / (Globals.CAMERA_CENTER_Y / Math.tan(Math.toRadians(Globals.CAMERA_VFOV / 2))))) + (Globals.CAMERA_VFOV/2);
+		return Math.toDegrees(Math.atan(getYPixelError() / yDegreeConstant)) + halfVertFOV;
 	}
 
 	public double getDistance() {
@@ -62,12 +61,16 @@ public class Vision extends Subsystem {
 	}
 
 	public void update() {
-		area = Robot.grip.getNumberArray("area", area);
-		centerX = Robot.grip.getNumberArray("centerX", centerX);
-		centerY = Robot.grip.getNumberArray("centerY", centerY);
-		width = Robot.grip.getNumberArray("width", width);
-		height = Robot.grip.getNumberArray("height", height);
-		solidity = Robot.grip.getNumberArray("solidity", solidity);
+		try {
+			if (Robot.grip.getNumberArray("centerX", centerX) != null) {
+				area = Robot.grip.getNumberArray("area", area);
+				centerX = Robot.grip.getNumberArray("centerX", centerX);
+				centerY = Robot.grip.getNumberArray("centerY", centerY);
+				width = Robot.grip.getNumberArray("width", width);
+				height = Robot.grip.getNumberArray("height", height);
+				solidity = Robot.grip.getNumberArray("solidity", solidity);
+			}
+		} catch (NullPointerException e) {}
 
 		SmartDashboard.putNumber("X Degree Error", getXDegreeError());
 		SmartDashboard.putNumber("X Pixel Error", getXPixelError());
