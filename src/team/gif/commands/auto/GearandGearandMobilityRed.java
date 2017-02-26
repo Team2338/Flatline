61025d656fc1625560486eb5e473bfe9bf437792
Gindtis <gindtjoshua@gmail.com>
2017-02-26 14:15:49 -0600

    GearandGearandMobility
    Base of autonomous with loaded-on gear, getting a second gear, placing
    second gear, and getting the extra mobility points. (99.999% likely to
    be over time).

---
+++ src/team/gif/commands/auto/GearandGearandMobilityRed.java
@@ -0,0 +1,32 @@
+package team.gif.commands.auto;
+
+import lib.gif.commands.CommandGroup;
+import team.gif.commands.GearRelease;
+import team.gif.commands.WaitCommand;
+
+public class GearandGearandMobilityRed extends CommandGroup {
+
+    public GearandGearandMobilityRed() {
+    	addSequential(new DriveStraightEnc(13815));
+    	addSequential(new GearRelease(true));
+    	addSequential(new DriveStraightEnc(-7440));
+    	addSequential(new GyroTurn(-90));
+    	addSequential(new DriveStraightEnc(110000));
+    	addSequential(new GyroTurn(90));
+    	addSequential(new DriveStraightEnc(240000));
+    	addSequential(new GyroTurn(-30));
+    	addSequential(new DriveStraightEnc(16578));
+    	addSequential(new WaitCommand(2.5));
+    	addSequential(new DriveStraightEnc(-16578));
+    	addSequential(new GyroTurn(30));
+    	addSequential(new DriveStraightEnc(-240000));
+    	addSequential(new GyroTurn(-90));
+    	addSequential(new DriveStraightEnc(110000));
+    	addSequential(new GyroTurn(90));
+    	addSequential(new DriveStraightEnc(7440));
+    	addSequential(new GearRelease(true));
+    	addSequential(new DriveStraightEnc(-7440));
+    	addSequential(new GyroTurn(-45));
+    	addSequential(new DriveStraightEnc(12400));
+    }
+}
