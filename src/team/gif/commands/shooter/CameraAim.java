c59836262c7c2a955264365e2a95444b37fce110
Occidentalis920 <occidentalis920@gmail.com>
2017-04-06 19:23:37 -0500

    Updated so CameraAim updates its error

--- src/team/gif/commands/shooter/CameraAim.java
+++ src/team/gif/commands/shooter/CameraAim.java
@@ -8,6 +8,7 @@ import team.gif.Robot;
 public class CameraAim extends Command {
 	
 	private PIDCalculator turretPID;
+	private double setpoint;
 	private double error;
 
 	public CameraAim() {
@@ -16,10 +17,11 @@ public class CameraAim extends Command {
 
 	protected void initialize() {
 		turretPID = new PIDCalculator(Globals.TURRET_P, Globals.TURRET_I, Globals.TURRET_D, Globals.TURRET_I_ZONE);
-		error = Globals.TURRET_ANGLE_TO_TICK * Robot.vision.getXDegreeError();
+		setpoint = Robot.turret.getPosition() - Globals.TURRET_ANGLE_TO_TICK * Robot.vision.getXDegreeError();
 	}
 
 	protected void execute() {
+		error = setpoint - Robot.turret.getPosition();
 		Robot.turret.set(turretPID.getOutput(error));
 	}
 
