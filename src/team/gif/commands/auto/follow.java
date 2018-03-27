e04bf6905109843c6e8ec4f89dc15a8176000e57
Enan456 <enan332@gmail.com>
2018-03-27 13:55:20 -0500

    Added limelight follow auto but now it will work 1.1

--- src/team/gif/commands/auto/follow.java
+++ src/team/gif/commands/auto/follow.java
@@ -7,12 +7,12 @@ import team.gif.commands.drivetrain.ResetGyro;
 import team.gif.commands.drivetrain.ShiftOmni;
 import team.gif.commands.drivetrain.ShifterLow;
 import team.gif.commands.shooter.ResetTurret;
-
+import team.gif.subsystems.Limelight;
 public class Follow extends CommandGroup {
 
     public Mobility() {
     	addParallel(new drive(true));
     	addSequential(new WaitCommand(0.4, true);
-    	addSequential(new DriveStraightEnc(Limelight.getdistance(), 0.75, 9));
+    	addSequential(new DriveStraightEnc(getdistance(), 0.75, 9));
     }
 }
